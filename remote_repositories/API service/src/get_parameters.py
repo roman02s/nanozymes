dbcbfa468b0152d666ad453f94a30db050030576
Sim Roman <r.sim@vk.team>
2023-10-18 23:38:43 +0300

    fix find_parameters

--- remote_repositories/API service/src/get_parameters.py
+++ remote_repositories/API service/src/get_parameters.py
@@ -1,8 +1,47 @@
+import numpy as np
 import pandas as pd
 
 # Заглушка
 df = pd.read_csv("data/nanozymes_extended.csv")
 
-def get_parameters(link: str) -> dict:
-    print(df[df["link"] == link])
-    return df[df["link"] == link].iloc[0].to_dict()
+def get_parameters(K_m: str = None, V_max: str = None) -> dict:
+    if K_m is None and V_max is None:
+        distance = None
+    elif K_m is None:
+        distance = lambda value: abs(float(value['Vmax, mM/s']) - float(V_max))
+    elif V_max is None:
+        distance = lambda value: abs(float(value['Km, mM']) - float(K_m))
+    else:
+        distance = lambda value: abs(float(value['Vmax, mM/s']) - float(V_max)) + abs(float(value['Km, mM']) - float(K_m))
+    # Создаем список для хранения расстояний между значениями и целевыми значениями
+    distances = []
+    print("in get_parameters")
+    # Проходимся по каждому значению в данных
+    for _, value in df.iterrows():
+        # Вычисляем расстояние между значениями K_m и V_max
+        try:
+            # print("value", value)
+            # print("value['Vmax, mM/s']", value['Vmax, mM/s'])
+            _distance = distance(value)
+            # Добавляем расстояние и значение в список distances
+            distances.append((_distance, value))
+        except ValueError:
+            continue
+    
+    # Сортируем список distances по расстоянию в порядке возрастания
+    distances.sort(key=lambda x: x[0])
+    print("distances")
+    print(
+        [{
+            "distance": distance,
+            ** value,
+        } for distance, value in distances[:5]
+    ]
+    )
+
+    # Возвращаем топ N наиболее похожих значений
+    return [{
+            "distance": distance,
+            ** value,
+        } for distance, value in distances[:5]
+    ]
